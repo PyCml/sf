81423e3b6081dab754893f32406631cfc56a066b
PyCml <py.calamia@gmail.com>
2015-04-19 15:04:52 +0200

    version 1.0.1

---
+++ src/testiAdvize/FrontBundle/Class/Methods.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pierre
+ * Date: 19/04/2015
+ * Time: 14:50
+ */
+namespace testiAdvize\FrontBundle\Class;
+
+class Methods
+{
+    /**
+     * @param $indexSource
+     * @param $find
+     * @param $source
+     * @param $boolResultat
+     * @return null
+     */
+    public static function searchNode($indexSource, $find, $source,$boolResultat)
+    {
+        $nodes = $source->find($find);
+        $res = null;
+        $index = 0;
+        foreach ($nodes as $node) {
+            if ($indexSource == $index && $boolResultat) {
+                $res = $node->plaintext;
+            }
+            elseif($indexSource == $index)
+            {
+                $res = $node;
+            }
+
+            $index = $index + 1;
+        }
+        return $res;
+    }
+}
--- src/testiAdvize/FrontBundle/Controller/DefaultController.php
+++ src/testiAdvize/FrontBundle/Controller/DefaultController.php
@@ -4,42 +4,22 @@ namespace testiAdvize\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sunra\PhpSimple\HtmlDomParser;
+use testiAdvize\FrontBundle\Class\Methods;;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
 
-
         $content =HtmlDomParser::file_get_html( "http://m.viedemerde.fr/" );
 
-        $nodes = $content->find('ul');
-        $res= null;
-        $index =0;
-        foreach($nodes as $node)
-        {
-            if($index==1)
-            {
-                $res = $node;
-
-            }
-            $index = $index +1;
-        }
-
-        $nodes = $res->find('p');
-        $res= null;
-        $index =0;
-        foreach($nodes as $node)
-        {
-            if($index==0)
-            {
-                $res = $node->plaintext;
-
-            }
-            $index = $index +1;
-        }
+        $ul = Methods::searchNode(1,'ul',$content,false);
+        $p = Methods::searchNode(0,'p',$ul,true);
 
 
-        return $this->render('testiAdvizeFrontBundle:Default:index.html.twig',array('dom'=>$res));
+
+
+        return $this->render('testiAdvizeFrontBundle:Default:index.html.twig',array('dom'=>$p));
     }
+
 }
